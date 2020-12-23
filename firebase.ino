311f3ec7f279c3ece60eb70c1fa1c6a914459be6
nabilaazalia <72654873+nabilaazalia@users.noreply.github.com>
2020-12-23 13:48:47 +0700

    Add files via upload

---
+++ firebase.ino
@@ -0,0 +1,105 @@
+#include <FirebaseESP8266.h>
+#include <FirebaseESP8266HTTPClient.h>
+#include <FirebaseFS.h>
+#include <FirebaseJson.h>
+
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+
+#define DHTPIN D5
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+// Set these to run example.
+#define FIREBASE_HOST "https://test-e7e86-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "lyKGVxv5voJFSPwsiCjZQKmlUT8nptuXMtk6hfAH"
+#define WIFI_SSID "R family"
+#define WIFI_PASSWORD "keren105"
+
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+
+
+FirebaseJson json;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DHT11 test!");
+  dht.begin();
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+
+void sensorUpdate(){
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("C  ,"));
+  Serial.print(f);
+  Serial.println(F("F  "));
+
+if (Firebase.setFloat(firebaseData, "/FirebaseIOT/temperature", t))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  if (Firebase.setFloat(firebaseData, "/FirebaseIOT/humidity", h))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+void loop() {
+  sensorUpdate();
+  
+}
